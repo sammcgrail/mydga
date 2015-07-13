7779f6c8961ed8d93ac72a7288cc790fa5fca318
sammcgrail <sammcgrail@gmail.com>
2015-07-13 16:18:02 -0400

    d3 files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
+
+  get 'graph/index'
+  get 'graph/data', :defaults => { :format => 'json' }
+
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +62,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
