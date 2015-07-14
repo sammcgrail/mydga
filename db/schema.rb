bb1ddf33466babdb6235df573f6bf2a22b7d7d59
sammcgrail <sammcgrail@gmail.com>
2015-07-14 11:19:21 -0400

    add transformers table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713225047) do
+ActiveRecord::Schema.define(version: 20150714151232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20150713225047) do
     t.decimal  "acetylene"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "transformers", force: :cascade do |t|
+    t.integer  "location_id", null: false
+    t.string   "name",        null: false
+    t.string   "type",        null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
