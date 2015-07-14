f10c5c1faccea517a360ba10740f31d9d7f01730
sammcgrail <sammcgrail@gmail.com>
2015-07-14 11:08:09 -0400

    samples table migration

--- db/migrate/20150713225047_create_samples.rb
+++ db/migrate/20150713225047_create_samples.rb
@@ -1,6 +1,20 @@
 class CreateSamples < ActiveRecord::Migration
   def change
     create_table :samples do |t|
+      t.integer :user_id, null: false
+      t.integer :transformer_id, null: false
+
+      t.string :date, null: false
+
+      t.decimal :hydrogen
+      t.decimal :oxygen
+      t.decimal :nitrogen
+      t.decimal :methane
+      t.decimal :carbon_monoxide
+      t.decimal :ethane
+      t.decimal :carbon_dioxide
+      t.decimal :ethylene
+      t.decimal :acetylene
 
       t.timestamps null: false
     end
