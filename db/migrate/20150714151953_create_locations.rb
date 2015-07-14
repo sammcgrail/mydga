56a4f0dd2956033f451ea2c0d1be7fe101763e77
sammcgrail <sammcgrail@gmail.com>
2015-07-14 11:21:49 -0400

    create locations table in db

---
+++ db/migrate/20150714151953_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :name, null: false
+      t.string :latitude
+      t.string :longitude
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714151232) do
+ActiveRecord::Schema.define(version: 20150714151953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "samples", force: :cascade do |t|
     t.integer  "user_id",         null: false
