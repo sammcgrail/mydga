9f8e94de04a6d135a7d51caf7fc1d93ce82ce638
sammcgrail <sammcgrail@gmail.com>
2015-07-23 17:28:29 -0400

    styles and stuff

--- app/assets/javascripts/sample_gauge_chart.js
+++ app/assets/javascripts/sample_gauge_chart.js
@@ -206,3 +206,58 @@ if ($("#sample_hydrogen_gauge_chart").length > 0) {
   }, 1400);
 
 }
+
+
+
+
+
+
+//
+// // christina code
+//
+// var sample_ethylene_gauge_chart = generateChart("Hydrogen")
+//
+//
+//
+// var generateChart = function(arg1, arg2) {
+//   c3.generate({
+//       bindto: document.getElementById('sample_hydrogen_gauge_chart'),
+//       data: {
+//           columns: [
+//               ['data', ]
+//           ],
+//           type: 'gauge',
+//           onclick: function (d, i) { console.log("onclick", d, i); },
+//           onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+//           onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+//       },
+//       gauge: {
+//          label: {
+//              format: function(value, ratio) {
+//                  return value ;
+//              },
+//              show: true // to turn off the min/max labels.
+//          },
+//      min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+//      max: 1800,  // 100 is default
+//      units: arg1,
+//      width: 39 // for adjusting arc thickness
+//       },
+//       color: {
+//           pattern: [ '#60B044','#F6C600', '#F97600',  '#FF0000',], // the four color levels for the percentage values.
+//           threshold: {
+//   //            unit: 'value', // percentage is default
+//   //            max: 200, // 100 is default
+//               values: [100, 700, 1800]
+//           }
+//       },
+//       size: {
+//           height: 180
+//       }
+//   });
+//   setTimeout(function () {
+//       sample_hydrogen_gauge_chart.load({
+//           columns: [['data', gon.hydrogen]]
+//       });
+//   }, 1000);
+// }
